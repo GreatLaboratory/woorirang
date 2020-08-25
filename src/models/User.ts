37ee631d660abb938d56d1b80f80838c9edb2181
Myung-gwan Kim <cartopia95@naver.com>
2020-08-25 09:28:04 +0900

    add - User DB 모델링

---
+++ src/models/User.ts
@@ -0,0 +1,27 @@
+import { Model, HasManyGetAssociationsMixin, Association } from 'sequelize';
+import Post from './Post';
+import Comment from './Comment';
+import * as bcrypt from 'bcrypt-nodejs';
+
+export default class User extends Model {
+    public id!: number;
+    public email!: string;
+    public password!: string;
+    public nickname!: string;
+    public mbti!: string;
+
+    validPassword (password: string) {
+        return bcrypt.compareSync(password, this.password);
+    }
+
+    public getPosts!: HasManyGetAssociationsMixin<Post>;
+    public getComments!: HasManyGetAssociationsMixin<Comment>;
+
+    public readonly createdAt!: Date;
+    public readonly updatedAt!: Date;
+
+    public static associations: {
+        posts: Association<User, Post>;
+        comments: Association<User, Comment>;
+    };
+}
