5a544ef96944ad49350ecefe7f4d5c2077f2fb00
Myung-gwan Kim <cartopia95@naver.com>
2020-08-27 08:58:43 +0900

    feat - db connection 생성

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-// import cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import cors from 'cors';
 import errorHandler from 'errorhandler';
 import express, { Application, Request, Response } from 'express';
 import helmet from 'helmet';
 import hpp from 'hpp';
-// import { Sequelize } from 'sequelize';
+import { Sequelize } from 'sequelize';
 import passport from 'passport';
 
-// import { init } from './models';
-// import { COOKIE_SECRET } from './config/secret';
+import { init } from './models/index';
+import { COOKIE_SECRET } from './config/secret';
 import { passportConfig } from './config/passport';
-// import userRouter from './routers/userRouter';
+import userRouter from './routers/userRouter';
 // import postRouter from './routers/postRouter';
 // import commentRouter from './routers/commentRouter';
 
@@ -22,28 +22,28 @@ class Server {
     // 생성자
     constructor () {
         this.app = express();
-        // this.connectDB();
+        this.connectDB();
         this.config();
         passportConfig(passport);
-        // this.routes();
+        this.routes();
     }
 
     // DB 연결
-    // private async connectDB (): Promise<void> {
-    //     const sequelize: Sequelize = init();
-    //     try {
-    //         await sequelize.authenticate();
-    //         await sequelize.sync();
-    //         // This will run .sync() only if database name ends with '_test'
-    //         // 이건 굉장히 파괴적인 옵션이라 test-dev서버에서 한번 데이터들 싹 갈아엎을 때 해야한다.
-    //         // await sequelize.sync({ force: true, match: /_test$/ });
-    //         console.log('DB 연결에 성공했습니다.');
-    //     } catch (err) {
-    //         console.log('DB 연결에 실패했습니다.');
-    //         console.log(err);
-    //         await sequelize.close();
-    //     }
-    // }
+    private async connectDB (): Promise<void> {
+        const sequelize: Sequelize = init();
+        try {
+            await sequelize.authenticate();
+            await sequelize.sync();
+            // This will run .sync() only if database name ends with '_test'
+            // 이건 굉장히 파괴적인 옵션이라 test-dev서버에서 한번 데이터들 싹 갈아엎을 때 해야한다.
+            // await sequelize.sync({ force: true, match: /_test$/ });
+            console.log('DB 연결에 성공했습니다.');
+        } catch (err) {
+            console.log('DB 연결에 실패했습니다.');
+            console.log(err);
+            await sequelize.close();
+        }
+    }
 
     // 기본 서버 설정 및 미들웨어 
     private config (): void {
@@ -62,16 +62,16 @@ class Server {
             extended: false, 
             limit: '200mb'
         }));
-        // this.app.use(cookieParser(COOKIE_SECRET));
+        this.app.use(cookieParser(COOKIE_SECRET));
         this.app.use(passport.initialize());
     }
 
     // 라우터
-    // private routes (): void {
-    //     this.app.use('/api/user', userRouter);
-    //     this.app.use('/api/post', postRouter);
-    //     this.app.use('/api/comment', commentRouter);
-    // }
+    private routes (): void {
+        this.app.use('/api/user', userRouter);
+        // this.app.use('/api/post', postRouter);
+        // this.app.use('/api/comment', commentRouter);
+    }
 
     // 서버 구동
     public start (): void {
@@ -83,7 +83,7 @@ class Server {
             res.status(200).send();
         });
         this.app.get('/test', (req: Request, res: Response) => {
-            res.status(200).json({ message: 'test success!!!333' });
+            res.status(200).json({ message: 'test success!!!333', today: new Date() });
         });
     }
 }
